9dc61bdb6ee850da346550902ad679d7af6ad140
Karthik PT <karthik.p@vuedata.in>
2023-12-23 11:00:43 +0530

    joy0

--- web/frontend/assets/icons/index.js
+++
@@ -1,3 +0,0 @@
-import ProfileIcon from "../icons/ProfileIcon";
-
-export { ProfileIcon };
--- web/frontend/components/SideNavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SideNavBar() {
-  return (
-    <div>
-      <h2 style={{ color: "red", fontSize: 30 }}>SideNavBar </h2>
-    </div>
-  );
-}
